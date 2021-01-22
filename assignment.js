c572ed1023d85d3f7c11061c65515aa660c76682
Debabrata Saha <sahadebabrata570@gmail.com>
2021-01-22 19:22:20 +0600

    gave space between every line and added console log

--- assignment.js
+++ assignment.js
@@ -4,19 +4,39 @@
 //my personal kilometerToMeter calculator
 
 function kilometerToMeter(kilometer) {
+
   var meter = kilometer * 1000;
+
+  if (kilometer < 0) {
+
+    console.log("Distance can not be negative");
+
+  }
+
   return meter;
 }
+
 
 
 
 //my personal budgetCalculator
 
 function budgetCalculator(watch, phone, laptop) {
+
   var totalWatchPrice = watch * 50;
+
   var totalPhonePrice = phone * 100;
+
   var totalLaptopPrice = laptop * 500;
+
   var total = totalWatchPrice + totalPhonePrice + totalLaptopPrice;
+
+  if (watch < 0 || mobile < 0 || laptop < 0) {
+
+    console.log("Price can not be negative");
+
+  }
+
   return total;
 }
 
@@ -25,40 +45,69 @@ function budgetCalculator(watch, phone, laptop) {
 
 
 function hotelCost(days) {
+
   var totalCost = 0;
+  
   if (days <= 10) {
+
     totalCost = days * 100;
   }
+
   else if (days <= 20) {
+
     var firstPart = 10 * 100;
+
     var remaining = days - 10;
+
     var secondPart = remaining * 80;
+
     totalCost = firstPart + secondPart;
+
   }
+
   else {
+
     var firstPart = 10 * 100;
+
     var secondPart = 10 * 80;
+
     var remaining = days - 20;
+
     var thirdPart = remaining * 50;
+
     totalCost = firstPart + secondPart + thirdPart;
+
   }
+
   return totalCost;
 }
 
+
+
 // megaFriend function
-function megaFriend(friends){
-    var max = friends[0];
-  for(var i = 0; i< friends.length; i++){
-    var element = friends[i];
-    if(element > max){
-      max = element;
-    }
+
+function megaFriend(friends) {
+
+  var max = friends[0]
+
+  for (var i = 0; i < friends.length; i++) {
+
+      var element = friends[i];
+
+      if (element.length > max.length) {
+
+          max = element;
+      }
   }
   return max;
 }
 
-// console.log(megaFriend(["shiful Islam Bappishiful Islam Bappishiful Islam Bappishiful Islam Bappishiful Islam Bappi","shiful Islam Bappishiful Islam Bappi" ,"saiful alam","Tawsif ahnaf","Joy Banik"]));
-// console.log(hotelCost(25));
+
+var friendList = ["Jhankar Mahbub","Programming-hero-community","Shiful Islam Bappi", "saiful Alam", "Joy banik","tawsif ahnaf"];
+
+
+console.log(megaFriend(friendList));
+
 
 
 
